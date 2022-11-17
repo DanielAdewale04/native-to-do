f11e573e4b491ec68be8f510601f1d97613ce232
Daniel Adewale <adewaled03@gmail.com>
2022-11-17 08:35:57 +0100

    Lottie animation

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-import { useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
+import { Animated, Easing } from 'react-native';
 import { StyleSheet, Text, View, TextInput, Button, Image, TouchableHighlight } from 'react-native';
 import ModalView from './modal';
 import { StatusBar } from 'expo-status-bar';
 import { mainAppStyle } from './styles';
+import Lottie from 'lottie-react-native';
 
 export default function App() {
 
+  const [loading, setLoading] = useState(false);
   const [openModal, setOpenModal] = useState(false);
   const [listItem, setListItem] = useState([])
   const [inputItem, setInputItem] = useState('')
@@ -18,10 +21,27 @@ export default function App() {
     setOpenModal(!openModal)
   }
 
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false);
+    }, 4000)
+  }, [])
+
+  const animationProgress = useRef(new Animated.Value(0))
+
+  useEffect(() => {
+    Animated.timing(animationProgress.current, {
+      toValue: 1,
+      duration: 4000,
+      easing: Easing.linear,
+      useNativeDriver: false
+    }).start();
+  }, [])
+
   // Handle Add Item
   const handleAddItem = () => {
     if (inputItem !== ''){
-      console.log('it is working')
       mockArray.push(inputItem);
       setListItem(mockArray)
       setInputItem('')
@@ -48,56 +68,70 @@ export default function App() {
   return (
     <>
       <StatusBar style='inverted' />
-      <View style={styles.container}>
-        <View style={mainAppStyle.container}>
-          <View style={mainAppStyle.flexApart}>
-            <Text style={mainAppStyle.HeaderText}>To-do List</Text>
-            <Text 
-              style={mainAppStyle.noItemMsg}
-              onPress={() => toggleModal()}
-            >Add</Text>
-          </View>
+      {
+        loading ?
 
-          {/* Modal Section */}
+          <Lottie 
+            progress={animationProgress.current}
+            source={require('./assets/loader.json')} 
+            backgroundColor='#000'
+            loop={true}
+            autoPlay={true}
+          />
 
-            <ModalView 
-              visible={openModal}
-              handleVisible={toggleModal}
-              inputItem={inputItem}
-              handleAddItem={handleAddItem}
-              handleInput={handleInput}
-            />
+          :
+      
+          <View style={styles.container}>
+            <View style={mainAppStyle.container}>
+              <View style={mainAppStyle.flexApart}>
+                <Text style={mainAppStyle.HeaderText}>To-do List</Text>
+                <Text 
+                  style={mainAppStyle.noItemMsg}
+                  onPress={() => toggleModal()}
+                >Add</Text>
+              </View>
 
-          {/* End of Modal section */}
-          
-          <View style={mainAppStyle.mainItemWrap}>
-            <Text style={mainAppStyle.notifier}>Tap on item to delete!</Text>
-            {
-              listItem && listItem.length > 0 ?
-                listItem.map((item, index) => (
-                  <TouchableHighlight key={index} onPress={() => handleDelete(index)}>
-                    <View 
-                      style={mainAppStyle.itemWrap}
-                    >
-                      <Text style={mainAppStyle.itemFont}>{item}</Text>
+              {/* Modal Section */}
+
+                <ModalView 
+                  visible={openModal}
+                  handleVisible={toggleModal}
+                  inputItem={inputItem}
+                  handleAddItem={handleAddItem}
+                  handleInput={handleInput}
+                />
+
+              {/* End of Modal section */}
+              
+              <View style={mainAppStyle.mainItemWrap}>
+                <Text style={mainAppStyle.notifier}>Tap on item to delete!</Text>
+                {
+                  listItem && listItem.length > 0 ?
+                    listItem.map((item, index) => (
+                      <TouchableHighlight key={index} onPress={() => handleDelete(index)}>
+                        <View 
+                          style={mainAppStyle.itemWrap}
+                        >
+                          <Text style={mainAppStyle.itemFont}>{item}</Text>
+                        </View>
+                      </TouchableHighlight>
+                    ))
+
+                    : 
+                    
+                    <View style={mainAppStyle.imageWrap}>
+                      <Image 
+                        style={mainAppStyle.emptyImg}
+                        source={require('./assets/box.png')}
+                      />
+                      <Text style={mainAppStyle.noItemMsg}>Nothing to show yet!</Text>
                     </View>
-                  </TouchableHighlight>
-                ))
-
-                : 
+                }
                 
-                <View style={mainAppStyle.imageWrap}>
-                  <Image 
-                    style={mainAppStyle.emptyImg}
-                    source={require('./assets/box.png')}
-                  />
-                  <Text style={mainAppStyle.noItemMsg}>Nothing to show yet!</Text>
-                </View>
-            }
-            
+              </View>
+            </View>
           </View>
-        </View>
-      </View>
+        }
     </>
   );
 }
